435034f5606c240aab4c62a8ba6f58bd9aec2b40
Trisha Saple <trisha.saple@northgateps.com>
2022-10-08 19:30:24 +0530

    added about and contactus pages

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,6 @@
+class WelcomesController < ApplicationController
+  def index
+  end
+  def contactus
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'about/index'  
+  get 'welcomes/index'
+  get 'contactus', to: 'welcomes#contactus'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "welcomes#index"
 end
