62ef43a46cd0416b42a4ec5bb087bb51347dcc3f
Trisha Saple <trisha.saple@northgateps.com>
2022-10-09 15:23:15 +0530

    articles table created through migration command

---
+++ db/migrate/20221009094253_create_articles.rb
@@ -0,0 +1,9 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
