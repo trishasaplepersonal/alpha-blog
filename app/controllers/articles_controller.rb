38f6a1f004d232a2ac7a51c6af6a4efc1e989b6a
Trisha Saple <trisha.saple@northgateps.com>
2022-10-11 12:04:14 +0530

    Edit article commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,33 @@ class ArticlesController < ApplicationController
     end    
 
     def new
-
+        @article = Article.new
     end    
 
+    def edit
+        @article = Article.find(params[:id])                
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        #white listing the params
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article updated successfully"
+            redirect_to @article
+        else
+            render 'edit'    
+        end    
+    end        
+
     def create
+        #white listing the params
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
+        if @article.save
+        flash[:notice]            = "Article save successfully."
         redirect_to @article
+        else            
+        render 'new'
+        end    
     end    
         
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'about/index'  
   get 'welcomes/index'
   get 'contactus', to: 'welcomes#contactus'
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
