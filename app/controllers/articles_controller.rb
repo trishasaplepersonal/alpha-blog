a9c4d3271ee7a1878e436c417ab32797e9616a69
Trisha Saple <trisha.saple@northgateps.com>
2022-10-11 21:57:31 +0530

    code refactoring done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def index
         @articles = Article.all 
     end
 
     def show
-        @article = Article.find(params["id"])
+        
     end    
 
     def new
@@ -13,13 +15,12 @@ class ArticlesController < ApplicationController
     end    
 
     def edit
-        @article = Article.find(params[:id])                
+                     
     end
     
-    def update
-        @article = Article.find(params[:id])
+    def update        
         #white listing the params
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article updated successfully"
             redirect_to @article
         else
@@ -29,7 +30,7 @@ class ArticlesController < ApplicationController
 
     def create
         #white listing the params
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
         flash[:notice]            = "Article save successfully."
         redirect_to @article
@@ -38,11 +39,20 @@ class ArticlesController < ApplicationController
         end    
     end    
 
-    def destroy        
-        @article = Article.find(params[:id])
+    def destroy
         # destory doesn't needs to be saved
         @article.destroy
         redirect_to articles_path
+    end    
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end    
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end    
         
 end
