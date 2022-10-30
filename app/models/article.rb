932072494be29dec8575c9f88678cdef4fa94bac
Trisha Saple <trisha.saple@northgateps.com>
2022-10-30 21:45:21 +0530

    Validation changes done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
     validates :title,  presence: true, length: { minimum: 10, maximum: 50 }
     validates :description,  presence: true, length: { minimum: 10, maximum: 200 }
-    belongs_to :users
+    belongs_to :user
 end    
