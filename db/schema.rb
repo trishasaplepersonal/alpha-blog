95bbc4f68c0500f1b37a649cb48d4e36a67aaa13
Trisha Saple <trisha.saple@northgateps.com>
2022-10-30 19:51:20 +0530

    user model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_094616) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_133051) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
