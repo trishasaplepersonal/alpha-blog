e9b53466ef1be899a32cc5a509eab4b635bfeb9b
Trisha Saple <trisha.saple@northgateps.com>
2022-10-30 22:10:29 +0530

    user id defaulted

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,8 @@ class ArticlesController < ApplicationController
 
     def create
         #white listing the params
-        @article = Article.new(article_params)
+        @article = Article.new(article_params)        
+        @article.user = User.first
         if @article.save
         flash[:notice]            = "Article save successfully."
         redirect_to @article
