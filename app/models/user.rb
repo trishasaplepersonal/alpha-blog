b877eb2d0dcd7c6fb4d4933704bab2319d35cadf
Trisha Saple <trisha.saple@northgateps.com>
2023-01-03 18:40:21 +0530

    Securing password changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    has_many :articles
+    before_save { self.username = username.downcase }
+    has_many :articles    
     validates :username, uniqueness: {case_sensitive: false}, presence: true, length: {minimum:5, maximum: 20}
     validates :email, uniqueness: {case_sensitive: false}, presence: true, length: {maximum: 100}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
     message: "Enter a valid email address" }
+    has_secure_password
 end
