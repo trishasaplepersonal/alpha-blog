1f24fd4ebdc0c5a0424c9675ac050e3de15d55e1
Trisha Saple <trisha.saple@northgateps.com>
2023-01-04 15:54:49 +0530

    Signing up users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    def index
+        @users = User.all
+    end
+    def show
+        
+    end   
+    def new
+        @user = User.new
+    end    
+
+    def edit
+                     
+    end
+    def update        
+        #white listing the params
+        if @user.update(user_params)
+            flash[:notice] = "User updated successfully"
+            redirect_to @user
+        else
+            render 'edit'    
+        end    
+    end        
+
+    def create
+        #white listing the params
+        @user = User.new(user_params)                        
+        if @user.save            
+        flash[:notice] = "#{@user.username} !! Welcome to alpha blog."
+        redirect_to articles_path
+        else                        
+        render 'new'
+        end    
+    end
+    
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end    
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'about/index'  
   get 'welcomes/index'
   get 'contactus', to: 'welcomes#contactus'
+  get 'signup', to: 'users#new' 
+  resources :users, except: [:new] 
   resources :articles #, only: [:index, :show, :new, :create, :edit, :update, :delete]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
